a495244971e9a1217ad6effb1cb39e8d8f92dc02
Alizwa Ntlai <ntlaializwa@gmail.com>
2024-02-01 14:42:49 +0200

    Added api's and js functionality

--- src/index.js
+++ src/index.js
@@ -1,8 +1,16 @@
+function searchCity(city) {
+  let apiKey = "1eafo8c549253t7ecbdfbc850a6a8763";
+  let apiUrl =
+    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
+  console.log(apiUrl);
+}
+
 function searchBarElement(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#input-search");
-  let cityElement = document.querySelector("#weather-app-city");
+  let cityElement = document.querySelector("#city");
   cityElement.innerHTML = searchInput.value;
+  searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
