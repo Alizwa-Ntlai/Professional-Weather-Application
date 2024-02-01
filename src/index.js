3ef4de83253ea3e2308434d96f9453a8aeb7f498
Alizwa Ntlai <ntlaializwa@gmail.com>
2024-02-01 22:05:47 +0200

    Added weather data for more functionality

--- src/index.js
+++ src/index.js
@@ -2,10 +2,37 @@ function updateWeather(response) {
   let temperatureElement = document.querySelector("#temperature");
   let temperature = response.data.temperature.current;
   let cityElement = document.querySelector("#city");
+  let cityConditions = document.querySelector("#description");
+  let condition = response.data.condition.description;
+  let humidityElement = document.querySelector("#humidity");
+  let humidity = response.data.temperature.humidity;
+  let speedElement = document.querySelector("#speed");
+  let speed = response.data.wind.speed;
+  let timeElement = document.querySelector("#time");
+  let date = new Date(response.data.time * 1000);
+  timeElement.innerHTML = formatDate(date);
+  speedElement.innerHTML = speed + "km/h";
+  humidityElement.innerHTML = humidity + "%";
+  cityConditions.innerHTML = condition;
   cityElement.innerHTML = response.data.city;
   temperatureElement.innerHTML = Math.round(temperature);
 }
-
+function formatDate(date) {
+  let minutes = date.getMinutes();
+  let hours = date.getHours();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+  if (minutes < 10) minutes = `0${minutes}`;
+  return `${day} ${hours}:${minutes}`;
+}
 function searchCity(city) {
   let apiKey = "1eafo8c549253t7ecbdfbc850a6a8763";
   let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
