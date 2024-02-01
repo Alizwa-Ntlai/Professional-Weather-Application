6259616c17fe3682c46b01021f17c3f294c8ddca
Alizwa Ntlai <ntlaializwa@gmail.com>
2024-02-01 15:11:10 +0200

    Fixed api functionality and added it to the inner HTML

--- src/index.js
+++ src/index.js
@@ -1,17 +1,23 @@
+function updateWeather(response) {
+  let temperatureElement = document.querySelector("#temperature");
+  let temperature = response.data.temperature.current;
+  let cityElement = document.querySelector("#city");
+  cityElement.innerHTML = response.data.city;
+  temperatureElement.innerHTML = Math.round(temperature);
+}
+
 function searchCity(city) {
   let apiKey = "1eafo8c549253t7ecbdfbc850a6a8763";
-  let apiUrl =
-    "https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric";
-  console.log(apiUrl);
+  let apiUrl = `https://api.shecodes.io/weather/v1/current?query=${city}&key=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(updateWeather);
 }
 
 function searchBarElement(event) {
   event.preventDefault();
   let searchInput = document.querySelector("#input-search");
-  let cityElement = document.querySelector("#city");
-  cityElement.innerHTML = searchInput.value;
   searchCity(searchInput.value);
 }
 
 let searchForm = document.querySelector("#search-form");
 searchForm.addEventListener("submit", searchBarElement);
+searchCity("Cape Town");
